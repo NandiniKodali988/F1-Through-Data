8e3f8159b2c0f0d46ff9403105debac6fe6edb79
NandiniKodali988 <nandinikodali5@outlook.com>
2024-12-17 00:45:12 -0500

    final commits

--- build.sh
+++ build.sh
@@ -49,10 +49,11 @@ fi
 
 
 # PUSH WEBSITE TO GU DOMAINS 
-# printf 'Would you like to push to GU domains? (y/n)? '
-# read answer
-# if [ "$answer" != "${answer#[Yy]}" ] ;then 
-#     rsync -alvr --delete _site/* jfhgeorg@gtown.reclaimhosting.com:/home/jfhgeorg/public_html/dsan-5000/
-# else
-#     echo NOT PUSHING TO GU DOMAINS!
-# fi
+printf 'Would you like to push to GU domains? (y/n)? '
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
+    rsync -alvr --delete _site/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/dsan-5000/
+
+else
+    echo NOT PUSHING TO GU DOMAINS!
+fi
