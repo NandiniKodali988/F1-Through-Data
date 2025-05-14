06399dbe42e297c66c4459ce165648649da6cc1c
NandiniKodali988 <nandinikodali5@outlook.com>
2025-05-13 22:17:01 -0400

    delete extra files and polish content

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 printf 'Would you like to push to GU domains? (y/n)? '
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
-    rsync -alvr --delete _site/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/dsan-5000/
+    #rsync -alvr --delete _site/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/dsan-5000/
+    rsync -alvr --delete _site/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/f1/
 
 else
     echo NOT PUSHING TO GU DOMAINS!
